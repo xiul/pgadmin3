bfdeaa35195fcda4e70cdf78adba9432e6dda5ce
Luis Ochoa <ziul1979@gmail.com>
2011-06-01 09:38:51 -0430

    dditems->locators done.

--- pgadmin/dd/dditems/locators/ddRemoveTableLocator.cpp
+++ pgadmin/dd/dditems/locators/ddRemoveTableLocator.cpp
@@ -1,11 +1,12 @@
 //////////////////////////////////////////////////////////////////////////
 //
 // pgAdmin III - PostgreSQL Tools
-// RCS-ID:      $Id: gqbView.cpp 8268 2010-04-15 21:49:27Z xiul $
-// Copyright (C) 2002 - 2010, The pgAdmin Development Team
+//
+// Copyright (C) 2002 - 2011, The pgAdmin Development Team
 // This software is released under the PostgreSQL Licence
 //
-// ddRemoveTableLocator.cpp
+// ddRemoveColLocator.cpp - Locate table delete button inside a table.
+//
 //////////////////////////////////////////////////////////////////////////
 
 #include "pgAdmin3.h"
--- pgadmin/include/dd/dditems/locators/ddRemoveTableLocator.h
+++ pgadmin/include/dd/dditems/locators/ddRemoveTableLocator.h
@@ -1,11 +1,11 @@
 //////////////////////////////////////////////////////////////////////////
 //
 // pgAdmin III - PostgreSQL Tools
-// RCS-ID:      $Id: gqbObject.h 8189 2010-02-25 22:10:21Z dpage $
-// Copyright (C) 2002 - 2010, The pgAdmin Development Team
+//
+// Copyright (C) 2002 - 2011, The pgAdmin Development Team
 // This software is released under the PostgreSQL Licence
 //
-// gqbObject.h - 
+// ddRemoveColLocator.cpp - Locate table delete button inside a table.
 //
 //////////////////////////////////////////////////////////////////////////
 
